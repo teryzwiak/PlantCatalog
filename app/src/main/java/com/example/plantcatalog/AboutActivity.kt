dc2e84064987271d0633fcbeef114385172ae11d
teryzwiak <patryk.jozwiak2001@gmail.com>
2024-01-31 23:50:15 +0100

    Option Acivity fix + About Activity

---
+++ app/src/main/java/com/example/plantcatalog/AboutActivity.kt
@@ -0,0 +1,19 @@
+package com.example.plantcatalog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.plantcatalog.databinding.AboutUsSectionBinding
+
+
+class AboutActivity : AppCompatActivity() {
+
+    private lateinit var binding: AboutUsSectionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = AboutUsSectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
